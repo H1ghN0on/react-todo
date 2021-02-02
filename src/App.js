ccfc40b64982695978b910566588a5f22b4ff096
h1ghn0on <you@example.com>
2021-02-02 14:05:39 +0300

    layout deployed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <form className="todo-control">
+          <label><input className="header-input" type="text" placeholder="Какие планы?"></input></label>
+          <button className="header-button" id="add"></button>
+        </form>
       </header>
+      <div className="todo-container">
+        <ul className="todo todo-list" id="todo">
+          <li className="todo-item">
+            <span className="text-todo">Купить МиниКупер</span>
+            <div className="todo-buttons">
+              <button className="todo-remove"></button>
+              <button className="todo-complete"></button>
+            </div>
+          </li>
+          <li className="todo-item">
+            <span className="text-todo">Выполнить усложненное задание</span>
+            <div className="todo-buttons">
+              <button className="todo-edit"></button>
+              <button className="todo-remove"></button>
+              <button className="todo-complete"></button>
+            </div>
+          </li>
+        </ul>
+        <ul className="todo todo-completed" id="completed">
+          <li className="todo-item">
+            <span className="text-todo">Продать Мазду</span>
+            <div className="todo-buttons">
+              <button className="todo-remove"></button>
+              <button className="todo-complete"></button>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
