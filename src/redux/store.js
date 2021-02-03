7ac7ec5a4e5ecba525ef7be55d72da40a15095a2
h1ghn0on <you@example.com>
2021-02-03 13:43:16 +0300

    localstorage added

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,10 @@ import thunk from 'redux-thunk';
 import rootReducer from "./reducers";
 
 const composeEnhancers =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer,
+const store = createStore(rootReducer, JSON.parse(localStorage['redux-store']),
 composeEnhancers(applyMiddleware(thunk)));
+
+store.subscribe(() => {
+    localStorage['redux-store'] = JSON.stringify(store.getState());
+})
 export default store;
